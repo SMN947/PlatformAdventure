3a7ab886d36d3358775f8a467c7e060eee5c6a6e
SMN947 <betum98@gmail.com>
2021-01-23 21:15:09 -0500

    Added listener to play button and high score handler

--- app/src/main/java/com/smn947/platformadventure/MainActivity.java
+++ app/src/main/java/com/smn947/platformadventure/MainActivity.java
@@ -2,6 +2,10 @@ package com.smn947.platformadventure;
 
 import android.app.*;
 import android.os.*;
+import android.view.*;
+import android.content.*;
+import android.widget.*;
+import android.util.*;
 
 public class MainActivity extends Activity 
 {
@@ -10,5 +14,17 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+		
+		findViewById(R.id.play).setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View view) {
+					System.out.println("BTN Clicked");
+					startActivity(new Intent(MainActivity.this, GameActivity.class));
+				}
+			});
+
+        TextView highScoreTxt = findViewById(R.id.highScoreTxt);
+        final SharedPreferences prefs = getSharedPreferences("game", MODE_PRIVATE);
+        highScoreTxt.setText("HighScore: " + prefs.getInt("highscore", 0));
     }
 }
