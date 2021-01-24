7095c58ddb1817366c75c2b48151ed4043f04ce7
SMN947 <betum98@gmail.com>
2021-01-23 21:17:32 -0500

    Added game activity

---
+++ app/src/main/java/com/smn947/platformadventure/GameActivity.java
@@ -0,0 +1,36 @@
+package com.smn947.platformadventure;
+import android.os.*;
+import android.view.*;
+import android.graphics.*;
+import android.app.*;
+
+public class GameActivity extends Activity{
+	private GameView gameView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Point point = new Point();
+        getWindowManager().getDefaultDisplay().getSize(point);
+
+        gameView = new GameView(this, point.x, point.y);
+
+        setContentView(gameView);
+    }
+
+    
+	@Override
+    protected void onPause() {
+        super.onPause();
+        gameView.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        gameView.resume();
+    }
+}
